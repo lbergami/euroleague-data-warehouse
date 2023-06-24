3632cdc26ea6ad651f2af9b00a15a2c402c5ca5f
lorenzo.bergami22@gmail.com <lorenzo.bergami22@gmail.com>
2023-06-24 14:41:58 +0000

    staging data

--- dbt/BigQuery/models/staging/src_box_scores.sql
+++ dbt/BigQuery/models/staging/src_box_scores.sql
@@ -1,1 +1,44 @@
-select * from  `drazen.euroleague_data.box_scores_data`
+with 
+
+source as (
+
+    select * from {{ source('euroleague_data', 'box_scores_data') }}
+
+),
+
+renamed as (
+
+    select
+        player_id,
+        isstarter,
+        team,
+        dorsal,
+        player,
+        minutes,
+        points,
+        fieldgoalsmade2,
+        fieldgoalsattempted2,
+        fieldgoalsmade3,
+        fieldgoalsattempted3,
+        freethrowsmade,
+        freethrowsattempted,
+        offensiverebounds,
+        defensiverebounds,
+        totalrebounds,
+        assistances,
+        steals,
+        turnovers,
+        blocksfavour,
+        blocksagainst,
+        foulscommited,
+        foulsreceived,
+        valuation,
+        flaghometeam,
+        game_id,
+        season
+
+    from source
+
+)
+
+select * from renamed
--- dbt/BigQuery/models/staging/src_shots.sql
+++ dbt/BigQuery/models/staging/src_shots.sql
@@ -1,1 +1,36 @@
-select * from  `drazen.euroleague_data.shots_data`
+with 
+
+source as (
+
+    select * from {{ source('euroleague_data', 'shots_data') }}
+
+),
+
+renamed as (
+
+    select
+        num_anot,
+        team,
+        id_player,
+        player,
+        id_action,
+        action,
+        points,
+        coord_x,
+        coord_y,
+        zone,
+        fastbreak,
+        second_chance,
+        points_off_turnover,
+        minute,
+        console,
+        points_home,
+        points_away,
+        game_id,
+        season
+
+    from source
+
+)
+
+select * from renamed
