8960f87cdae99a18513fb5c9ad6e4802bdedc62f
lorenzo.bergami22@gmail.com <lorenzo.bergami22@gmail.com>
2023-09-16 13:18:26 +0000

    amend sample models following change in sources

--- dbt/BigQuery/models/marts/sample_game/dim_game_teams_sample.sql
+++ dbt/BigQuery/models/marts/sample_game/dim_game_teams_sample.sql
@@ -5,7 +5,7 @@ source_table as (
 
     select *
     from {{ ref('dim_games_and_teams') }}
-    where season = '2022-23' and (game_id = 1 or game_id = 2 or game_id = 3 or game_id = 4)
+    where season = 2022 and (game_id = 1 or game_id = 2 or game_id = 3 or game_id = 4)
 
 ) 
 select * 
