3d04c96e4bc5df022b28cbce30bd95db4ed4bf73
lorenzo.bergami22@gmail.com <lorenzo.bergami22@gmail.com>
2023-10-02 16:37:30 +0000

    Add singular tests on shooting percentages

---
+++ dbt/BigQuery/tests/assert_sum_shooting_pct_equal_1.sql
@@ -0,0 +1,36 @@
+
+/* This singular test checks whether 'shooting_pct' per game and team correctly sums up to 1  */
+
+with 
+
+missed_shots_pct as (
+
+    select
+        isz.season,
+        isz.game_id, 
+        isz.team_code,
+        l.shot_zone_label,
+        count(case when isz.made_shot = 0 then isz.shot_id end) / 
+            (count(case when isz.made_shot = 1 then isz.shot_id end) + count(case when isz.made_shot = 0 then isz.shot_id end)) 
+            as missed_shots_pct
+    from {{ ref('int_shooting_zones') }} as isz
+    /* Join the lookup with clean label for shooting areas */
+    left join  {{ ref('dim_shot_zones_labels') }} l
+        on isz.shot_zone_agg = l.shot_zone_agg
+    group by isz.season, isz.game_id, isz.team_code, l.shot_zone_label       
+
+
+
+)
+
+select 
+    fsp.season, 
+    fsp.game_id, 
+    fsp.team_code,
+    fsp.shot_zone_label, 
+    fsp.shooting_pct + msp.missed_shots_pct
+from {{ ref('fct_shooting_pct') }} as fsp
+left join missed_shots_pct as msp
+    on fsp.season = msp.season and fsp.game_id = msp.game_id and 
+        fsp.team_code = msp.team_code and fsp.shot_zone_label = msp.shot_zone_label
+where (fsp.shooting_pct + msp.missed_shots_pct) != 1     
---
+++ dbt/BigQuery/tests/assert_sum_shooting_selection_equal_1.sql
@@ -0,0 +1,11 @@
+
+/* This singular test checks whether 'shooting_selection_pct' per game and team correctly sums up to 1  */
+
+select 
+    season, 
+    game_id, 
+    team_code, 
+    round(sum(shooting_selection_pct), 5) as tot_rounded
+from {{ ref('fct_shooting_pct') }}
+group by 1,2,3
+having tot_rounded < 1
